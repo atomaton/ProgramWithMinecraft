16a48553d23bb2c690ec45217ae4c8c97e4cf459
Matthew Martin <atomaton@gmail.com>
2017-02-17 13:12:39 -0500

    Update brokenWall.py

--- chapter10-forLoops/brokenWall.py
+++ chapter10-forLoops/brokenWall.py
@@ -5,27 +5,27 @@ import random
 
 
 def brokenBlock():
-    brokenBlocks = [48, 67, 4, 4, 4, 4]
-    block = random.choice(brokenBlocks)
+    brokenBlocks = [48, 67, 4, 4, 4, 4] #moss stone, cobblestone stairs, cobblestone
+    block = random.choice(brokenBlocks) #choose one of the 3 possible blocks with 4 of them being the same
     return block
 
-pos = mc.player.getTilePos()
+pos = mc.player.getTilePos() #retrieves the location of the player
 x, y, z = pos.x, pos.y, pos.z
 
-brokenWall = []
-height, width = 5, 10
+brokenWall = [] #creates an empty list called "brokenWall"
+height, width = 5, 10 #assigns numbers to variables
 
 # create the list of broken blocks
-for row in range(height):
-    brokenWall.append([])
-    for column in range(width):
-        block = brokenBlock()
+for row in range(height): #this should loop 5 tiems
+    brokenWall.append([]) #puts a block in the list above
+    for column in range(width): #this should loop 10 times
+        block = brokenBlock() #grabs a random block of moss stone, cobblestone stairs or cobblestone
         brokenWall[row].append(block)
 
 # set the blocks
-for row in brokenWall:
-    for block in row:
-        mc.setBlock(x, y, z, block)
-        x += 1
-    y += 1
-    x = pos.x
+for row in brokenWall: #this should loop 10 times.
+    for block in row: 
+        mc.setBlock(x, y, z, block) #put down a random block
+        x += 1 #go to the next space 
+    y += 1 #go up one block on the wall
+    x = pos.x #start at the beginning of the wall
