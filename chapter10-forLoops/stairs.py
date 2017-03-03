955cbb8b1225bee049f46161f77cb58bd6fe2a64
Matthew Martin <atomaton@gmail.com>
2017-03-03 15:37:44 -0500

    Update stairs.py

--- chapter10-forLoops/stairs.py
+++ chapter10-forLoops/stairs.py
@@ -1,10 +1,10 @@
 from mcpi.minecraft import Minecraft
 mc = Minecraft.create()
 
-pos = mc.player.getTilePos()
-x, y, z = pos.x, pos.y, pos.z
+pos = mc.player.getTilePos() #function that obtains the postition of the player and puts that location in the variable 'pos"
+x, y, z = pos.x, pos.y, pos.z #the position in the "pos" is then parsed out in to the 3 positions
 
-stairBlock = 53
+stairBlock = 53 #this sets the variable "stairBlock" to the woodstair which is block ID 53
 
-for step in range(10):
-    mc.setBlock(x + step, y + step, z, stairBlock)
+for step in range(10): #a for loop that creates 10 stairs as its run 10 times
+    mc.setBlock(x + step, y + step, z, stairBlock) # x is over one and y is up one
